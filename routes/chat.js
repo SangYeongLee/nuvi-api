3b81f9f02c0fa771d8a0325cba3596a6343e7108
SangYeongLee <dltkddud4403@naver.com>
2019-11-07 02:15:27 +0900

    register rendering page update

--- routes/chat.js
+++ routes/chat.js
@@ -9,11 +9,11 @@ router.use(express.json());
 
 //check chat bot id is already registered
 router.post('/regCheck',function(req,res,next){
-	var Nchat_id = req.body.userRequest.user.id;
-	var searchSQL = "SELECT chat_id FROM Students WHERE chat_id='"+Nchat_id+"' UNION SELECT chat_id from Parents WHERE chat_id='"+Nchat_id+"'";
-	var responseBody = {
-		version: "2.0",
-		template:{
+  var Nchat_id = req.body.userRequest.user.id;
+  var searchSQL = "SELECT chat_id FROM Students WHERE chat_id='"+Nchat_id+"' UNION SELECT chat_id from Parents WHERE chat_id='"+Nchat_id+"'";
+  var responseBody = {
+    version: "2.0",
+    template:{
                outputs:[{
                   basicCard:{
                     description: "아래의 링크로 회원가입을 진행해주세요.",
@@ -23,27 +23,27 @@ router.post('/regCheck',function(req,res,next){
                       webLinkUrl:"https://api.nuvi-labs.com/kakao/register?chat="
                     }]
                   }
-               }]
+               }]                        
              }
     };
 
     db.sequelize.query(searchSQL, { type: db.sequelize.QueryTypes.SELECT}).then(data=>{
-    	if(data.length!=0){
-    	   //block user already registered
+      if(data.length!=0){
+         //block user already registered
          responseBody = {version: "2.0", template:{outputs:[{simpleText:{text: "이미 등록된 사용자입니다.\n해당 계정으로 서비스를 이용하고 싶으시다면 아이디를 갱신해주시길 바랍니다."}}]}};
-			   res.status(200).send(responseBody);
-		  }else{
+         res.status(200).send(responseBody);
+      }else{
         //send webLink with chat_id and key
         var key = 0;
         for(i=0;i<Nchat_id.length;i++){
           key += Nchat_id.charCodeAt(i);
         }
         responseBody.template.outputs[0].basicCard.buttons[0].webLinkUrl+=Nchat_id+"&key="+key;
-			 res.status(200).send(responseBody);
-		  }
+       res.status(200).send(responseBody);
+      }
     }).catch(err=>{
-    	responseBody = {version: "2.0", template:{outputs:[{simpleText:{text: "Server Error"}}]}};
-		res.status(200).send(responseBody);
+      responseBody = {version: "2.0", template:{outputs:[{simpleText:{text: "Server Error"}}]}};
+    res.status(200).send(responseBody);
     });
 });
 
@@ -51,7 +51,7 @@ router.post('/regCheck',function(req,res,next){
 router.get('/register', function(req,res,next){
   var code_list=[];
   var school_list=[];
-
+  
   var uri = req.url;
   var chat_id = url.parse(uri,true).query.chat;
   var pkey = url.parse(uri,true).query.key;
@@ -121,7 +121,7 @@ router.post('/registerComplete', function(req,res,next){
     }).then(data=>{
       if(data.length!=0){
         var sid = data[0].id;
-
+        
         Parent.create(
           {name:req.body.name,chat_id:req.body.chat,SchoolId:req.body.school,StudentId:sid}
         ).then(data=>{
@@ -158,7 +158,7 @@ router.post('/reset',function(req,res,next){
                         webLinkUrl:"https://api.nuvi-labs.com/kakao/register?chat="+chat+"&key="+key
                       }]
                     }
-                 }]
+                 }]                        
                }
         };
   var key=0;
@@ -189,7 +189,7 @@ router.post('/reset',function(req,res,next){
                             webLinkUrl:"https://api.nuvi-labs.com/kakao/register?chat="+chat+"&key="+key
                           }]
                         }
-                     }]
+                     }]                        
                    }
           };
           res.status(200).send(responseBody);
@@ -214,10 +214,10 @@ router.post('/userInfo',function(req,res,next){
               + "WHERE stu.chat_id = '"+chat+"' and stu.schoolUniqueNum = sch.public_id;";
 
   Parent.findAll({where:{chat_id:chat}}).then(data=>{
-    if(data==0){
+    if(data.length==0){
       //chat id is not in parent table
       db.sequelize.query(infoSQL).then(data=>{
-        if(data==0){
+        if(data.length==0 || data[0].length==0){
           res.status(200).send(responseBody);
         }else{
           var grade = Number(data[0][0].code.substring(4,6));
@@ -225,7 +225,7 @@ router.post('/userInfo',function(req,res,next){
           var studentNum = Number(data[0][0].code.substring(8,10));
 
           responseBody = {
-            version: "2.0",
+            version: "2.0", 
             template:{
               outputs:[{
                 simpleText:{
@@ -238,26 +238,25 @@ router.post('/userInfo',function(req,res,next){
               }]
             }
           };
-          res.status(200).send(responseBody);
-        }
+          res.status(200).send(responseBody);  
+        }         
       }).catch(err=>{
         throw err;
       });
      }else{
       //chat id is in parent table
       console.log("parent id::  " + data[0].StudentId);
-      var infoSQL = "SELECT stu.name as name, stu.uniqueNum as code, sch.schoolName as school "
+      infoSQL = "SELECT stu.name as name, stu.uniqueNum as code, sch.schoolName as school "
               + "FROM nuvi_database.Students as stu, nuvi_database.Schools as sch "
               + "WHERE stu.id = "+data[0].StudentId+" and stu.schoolUniqueNum = sch.public_id;";
 
-      db.sequelize.query(infoSQL).then(data=>{
-        console.log(data);
+      db.sequelize.query(infoSQL).then(data=>{ 
         var grade = Number(data[0][0].code.substring(4,6));
         var classNum = Number(data[0][0].code.substring(6,8));
         var studentNum = Number(data[0][0].code.substring(8,10));
 
         responseBody = {
-          version: "2.0",
+          version: "2.0", 
           template:{
             outputs:[{
               simpleText:{
